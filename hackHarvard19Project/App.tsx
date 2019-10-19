9c64d7f9f3bdc8301c6316fc5c3a2ef9cafae5e4
Sakib Rasul <rhythms06@gmail.com>
2019-10-19 09:02:45 -0400

    firebase integration

--- hackHarvard19Project/App.tsx
+++ hackHarvard19Project/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Button } from 'react-native';
+import * as firebase from 'firebase';
+var app = firebase.initializeApp({
+  apiKey: "AIzaSyCW7Uf0qvFdDNfjgOhcfjnIwltW11z5v3s",
+  authDomain: "hackharvard19project.firebaseapp.com",
+  databaseURL: "https://hackharvard19project.firebaseio.com",
+  projectId: "hackharvard19project",
+  storageBucket: "hackharvard19project.appspot.com",
+  messagingSenderId: "637275919090",
+  appId: "1:637275919090:web:13a86e85b703e0cf0b766b"
+});
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>BandIt!</Text>
+      <Image
+        style={{ width: 50, height: 50 }}
+        source={require('./logo.png')}
+      />
+      <Text style={styles.titleText}>BandIt!</Text>
       <Button
         // onPress={onPressLearnMore}
         title="Sign In"
@@ -22,5 +37,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
   },
 });
