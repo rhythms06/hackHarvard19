8025ad94320e8c556a93f5919e5df9461501a2c8
Sakib Rasul <rhythms06@gmail.com>
2019-10-19 00:13:02 -0400

    button

--- hackHarvard19Project/App.tsx
+++ hackHarvard19Project/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>BandIt!</Text>
+      <Button
+        // onPress={onPressLearnMore}
+        title="Sign In"
+        color="#841584"
+        // hello
+      />
     </View>
   );
 }
