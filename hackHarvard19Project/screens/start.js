076253f767c026d34157c761c0b33d3d71ec155f
Sakib Rasul <rhythms06@gmail.com>
2019-10-19 13:09:36 -0400

    syntax

--- hackHarvard19Project/screens/start.js
+++ hackHarvard19Project/screens/start.js
@@ -3,20 +3,30 @@ import { StyleSheet, Text, View, Image, Button } from "react-native";
 import firebase from "firebase";
 import { SCREENS } from "../constants";
 
+var app = firebase.initializeApp({
+  apiKey: "AIzaSyCW7Uf0qvFdDNfjgOhcfjnIwltW11z5v3s",
+  authDomain: "hackharvard19project.firebaseapp.com",
+  databaseURL: "https://hackharvard19project.firebaseio.com",
+  projectId: "hackharvard19project",
+  storageBucket: "hackharvard19project.appspot.com",
+  messagingSenderId: "637275919090",
+  appId: "1:637275919090:web:13a86e85b703e0cf0b766b"
+});
+
 function Start(props) {
   const { navigation } = props;
 
   return (
     <View style={styles.container}>
-      <Image
-        style={{ width: 200, height: 200 }}
+      <Image style=
+        {{ width: 200, height: 200 }}
         source={require("./logo.png")}
       />
       <Text style={styles.titleText}>BandIt!</Text>
       <Button
-        onPress={() => navigation.navigate(SCREENS.SIGNIN)}
         title="Sign In"
         color="#841584"
+        onPress={() => navigation.navigate(SCREENS.LOGIN)}
       />
       <Button
         title="Sign Up"
