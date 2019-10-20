47b4f1336b6e1a40ba5cde5016202808d1110d2d
Sakib Rasul <rhythms06@gmail.com>
2019-10-20 09:14:56 -0400

    Scaffolding buttons in Home

--- hackHarvard19Project/screens/Home.js
+++ hackHarvard19Project/screens/Home.js
@@ -7,20 +7,69 @@ import { Platform, StyleSheet, Text, View, Alert, Button } from "react-native";
 export default class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {text: "", name: ""};
   };
 
  render() {
    return (
      <View style={styles.container}>
       <View style={styles.match}>
-        <Text>Olivia (Piano)</Text>
-        <Text>Jorge (Glockenspiel)</Text>
-        <Text>Barb (Tenor)</Text>
+        <Button
+          title="Olivia (Piano)"
+          onPress={(text) =>
+            {
+              this.name = "Olivia (Piano)";
+              console.log(this.name);
+            }
+          }
+        />
+        <Button
+          title="Barb (Tenor)"
+          onPress={(text) =>
+            {
+              this.name = "Barb (Tenor)";
+              console.log(this.name);
+            }
+          }
+        />
+        <Button
+          title="Jorge (Glockenspiel)"
+          onPress={(text) =>
+            {
+              this.name = "Jorge (Glockenspiel)";
+              console.log(this.name);
+            }
+          }
+        />
       </View>
       <View style={styles.match}>
-        <Text>Herman (Flute)</Text>
-        <Text>Ferguson (Guitar)</Text>
-        <Text>Franz (Sax)</Text>
+        <Button
+          title="Herman (Flute)"
+          onPress={(text) =>
+            {
+              this.name = "Herman (Flute)";
+              console.log(this.name);
+            }
+          }
+        />
+        <Button
+          title="Ferguson (Guitar)"
+          onPress={(text) =>
+            {
+              this.name = "Ferguson (Guitar)";
+              console.log(this.name);
+            }
+          }
+        />
+        <Button
+          title="Franz (Sax)"
+          onPress={(text) =>
+            {
+              this.name = "Franz (Sax)";
+              console.log(this.name);
+            }
+          }
+        />
       </View>
       <View style={styles.match}>
         <Button
